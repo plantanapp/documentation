abf07568ea3d2f1742ad9c125b1a6f6b4766746b
rhaiamz <cristian.popescu@avatar-soft.ro>
2020-05-05 14:00:44 +0300

    Added github links to edit pages directly on github.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,17 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/plantanapp/documentation/edit/master',
+            showLastUpdateAuthor: true,
+            showLastUpdateTime: true
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          'https://github.com/plantanapp/documentation/edit/master',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
