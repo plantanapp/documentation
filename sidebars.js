a9e9081586d4fde81e492e6ddb7144e625000899
Patrick Anderson <71077142+pande3@users.noreply.github.com>
2020-10-06 10:42:17 -0400

    Update to structure of sidebars.js and added modules/important-notes/… (#69)
    * Update to structure of sidebars.js and added modules/important-notes/secure-input.md
    
    Update to sidebars.js is to accomodate a location for this article and to plan ahead for adding detailed information for Modules
    
    * Linked the Allowed Attributes and CSS properties from the list of how the input is processed.

--- sidebars.js
+++ sidebars.js
@@ -1,35 +1,52 @@
 module.exports = {
-  someSidebar: {
-    'Getting Started': [
-      'audience',
-      'entities',
-      'execution-context',
-      'faq',
-      'roles',
-      'running-examples',
-      'users',
-      'versioning',
-      'workflows',
-      {
-        type: 'category',
-        label: 'Actions',
-        items: [
-          'actions/apply-tokens',
-          'actions/execute-token',
-          'actions/generate-pdf',
-          'actions/grant-user-role',
-          'actions/inject-data',
-          'actions/load-user',
-          'actions/load-users-from-sql',
-          'actions/merge-pdf',
-          'actions/overlay-pdf',
-          'actions/revoke-user-role',
-          'actions/run-sql',
-          'actions/sanitize-html',
-          'actions/send-email',
-          'actions/server-request',
-        ],
+  someSidebar: [
+    {
+      type: 'category',
+      label: 'Getting Started', 
+      items: [
+        'audience',
+        'entities',
+        'execution-context',
+        'roles',
+        'users',
+        'versioning',
+        'workflows',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Actions',
+      items: [
+        'faq',
+        'running-examples',
+        'actions/apply-tokens',
+        'actions/execute-token',
+        'actions/generate-pdf',
+        'actions/grant-user-role',
+        'actions/inject-data',
+        'actions/load-user',
+        'actions/load-users-from-sql',
+        'actions/merge-pdf',
+        'actions/overlay-pdf',
+        'actions/revoke-user-role',
+        'actions/run-sql',
+        'actions/sanitize-html',
+        'actions/send-email',
+        'actions/server-request',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Modules',
+      items: [
+        {
+          type: 'category',
+          label: 'Important Notes',
+          items: [
+            'modules/important-notes/secure-input',
+          ],
+        }
+      ],
       }
-    ]
-  },
+  ]
 };
